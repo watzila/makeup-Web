1ce2a13e6cf385bc4e72413e58ba7a83ac09cdaa
JianSong <a2792839a@gmail.com>
2020-11-27 10:21:09 +0800

    加入會員後端

--- makeup-web/src/components/js/user.jsx
+++ makeup-web/src/components/js/user.jsx
@@ -1,132 +1,143 @@
 import React, { Component } from "react";
-import Ajax from "./ajax";//和伺服連線
-import moment from 'moment' //日期格式化
+import Ajax from "./ajax"; //和伺服連線
 
 class User extends Component {
-	constructor(){
+	constructor() {
 		super();
 		this.state = {
-			data:null,
-			fnameDisabled :true,
-			fnameIsShow:true,
-			usernameIsShow:true,
-			usernameDisabled:true,
-			phoneIsShow:true,
-			phoneDisabled:true,
-			emailIsShow:true,
-			emailDisabled:true,
-			
-		}
+			data: null,
+			fnameDisabled: true,
+			fnameIsShow: true,
+			usernameIsShow: true,
+			usernameDisabled: true,
+			phoneIsShow: true,
+			phoneDisabled: true,
+			emailIsShow: true,
+			emailDisabled: true,
+		};
 		this.ajax = new Ajax();
-		
-		this.ajax.startListener("post", `/member/${JSON.parse(sessionStorage.getItem("member")).nickname}`, this.u, {nickname:JSON.parse(sessionStorage.getItem("member")).nickname});
+
+		this.ajax.startListener(
+			"post",
+			`/member/${JSON.parse(sessionStorage.getItem("member")).nickname}`,
+			this.u,
+			{ cId: JSON.parse(sessionStorage.getItem("member")).customer_id }
+		);
 		// console.log(this.u);
 	}
-	
 
-	u = (data) => {
+	u = data => {
 		this.setState({ data: data });
-	// 	console.log(data);
-	  }
+		// 	console.log(data);
+	};
 
-	handleClick = (e)=>{
-
-		switch(e.target.id){
+	handleClick = e => {
+		switch (e.target.id) {
 			case "fname":
-				this.setState({fnameIsShow:(this.state.fnameIsShow==false)?true:false})
-				this.setState({fnameDisabled:( this.state.fnameDisabled)?false:true});
+				this.setState({ fnameIsShow: this.state.fnameIsShow === false ? true : false });
+				this.setState({ fnameDisabled: this.state.fnameDisabled ? false : true });
 				break;
 			case "username":
 				// console.log(e.target.id)
-				this.setState({usernameIsShow:(this.state.usernameIsShow==false)?true:false})
-				this.setState({usernameDisabled:( this.state.usernameDisabled)?false:true});
+				this.setState({ usernameIsShow: this.state.usernameIsShow === false ? true : false });
+				this.setState({ usernameDisabled: this.state.usernameDisabled ? false : true });
 				break;
 			case "phone":
 				// console.log(e.target.id)
-				this.setState({phoneIsShow:(this.state.phoneIsShow==false)?true:false})
-				this.setState({phoneDisabled:( this.state.phoneDisabled)?false:true});
+				this.setState({ phoneIsShow: this.state.phoneIsShow === false ? true : false });
+				this.setState({ phoneDisabled: this.state.phoneDisabled ? false : true });
 				break;
 			case "email":
-				this.setState({emailIsShow:(this.state.emailIsShow==false)?true:false})
-				this.setState({emailDisabled:( this.state.emailDisabled)?false:true});
+				this.setState({ emailIsShow: this.state.emailIsShow === false ? true : false });
+				this.setState({ emailDisabled: this.state.emailDisabled ? false : true });
 				break;
 			default:
 				break;
 		}
-		
-	}
+	};
 
-	changeFname= (e)=>{
-		
-		this.setState({userAccount:e.target.value});
+	changeFname = e => {
+		this.setState({ userAccount: e.target.value });
 		console.log(e);
-	}
+	};
 
+	changeUsername = e => {
+		this.setState({ userName: e.target.value });
+	};
 
-	changeUsername= (e)=>{
-		this.setState({userName:e.target.value});
-	}
+	changePhone = e => {
+		this.setState({ userCellphone: e.target.value });
+	};
+	changeEmail = e => {
+		this.setState({ userEmail: e.target.value });
+	};
 
-	changePhone= (e)=>{
-		this.setState({userCellphone:e.target.value});
-	}
-	changeEmail= (e)=>{
-		this.setState({userEmail:e.target.value});
-	}
-	
 	// 取消input邊框及disable
-	noFocus=(e)=>{
+	noFocus = e => {
 		var dis = document.getElementById(e.target.id);
 		dis.disabled = "false";
-		dis.style.border = 0
+		dis.style.border = 0;
 		document.getElementById(e.target.id).disabled = dis.disabled;
 		document.getElementById(e.target.id).style.border = dis.style.border;
 		this.setState({});
-	}
-
-
-
+	};
 
 	render() {
 		return (
 			<div className="userForm">
 				<div>
 					<label htmlFor="fname">帳號：</label>
-					<input  style = {{border:this.state.fnameIsShow ?"0":"2px black solid"}}
-							disabled={this.state.data == null ? "" :this.state.fnameDisabled} 
-							placeholder={this.state.data == null ? "" : this.state.data[0].account}
-							onChange = {this.changeFname}  
-							type="text" id="fname" name="fname" autoComplete="off" >
-					</input>
-				{/* <button id="fname" onClick={this.handleClick} >編輯</button> */}
+					<input
+						style={{ border: this.state.fnameIsShow ? "0" : "2px black solid" }}
+						disabled={this.state.data == null ? "" : this.state.fnameDisabled}
+						placeholder={this.state.data == null ? "" : this.state.data[0].account}
+						onChange={this.changeFname}
+						type="text"
+						id="fname"
+						name="fname"
+						autoComplete="off"
+					></input>
+					{/* <button id="fname" onClick={this.handleClick} >編輯</button> */}
 				</div>
 
 				<hr />
 
 				<div>
 					<label htmlFor="username">姓名：</label>
-					<input 
-					 onBlur = {this.noFocus}
-					 style = {{border:this.state.usernameIsShow ?"0":"2px black solid"}}
-					 disabled={this.state.usernameDisabled} 
-					 placeholder={this.state.data == null ? "" : this.state.data[0].customerName}
-					 onChange = {this.changeUsername}
-					 type="text" id="username" name="username" autoComplete="off" />
-				<button id="username" onClick={this.handleClick} >編輯</button>
+					<input
+						onBlur={this.noFocus}
+						style={{ border: this.state.usernameIsShow ? "0" : "2px black solid" }}
+						disabled={this.state.usernameDisabled}
+						placeholder={this.state.data == null ? "" : this.state.data[0].customerName}
+						onChange={this.changeUsername}
+						type="text"
+						id="username"
+						name="username"
+						autoComplete="off"
+					/>
+					<button id="username" onClick={this.handleClick}>
+						編輯
+					</button>
 				</div>
 
 				<hr />
 
 				<div>
 					<label htmlFor="phone">手機：</label>
-					<input 
-					onBlur = {this.noFocus}
-					style = {{border:this.state.phoneIsShow ?"0":"2px black solid"}}
-					disabled={this.state.phoneDisabled} 
-					placeholder={this.state.data == null ? "" : this.state.data[0].cellPhone}
-					onChange = {this.changePhone}
-					type="text" id="phone" name="phone"  autoComplete="off" />
-				<button id="phone" onClick={this.handleClick} >編輯</button>
+					<input
+						onBlur={this.noFocus}
+						style={{ border: this.state.phoneIsShow ? "0" : "2px black solid" }}
+						disabled={this.state.phoneDisabled}
+						placeholder={this.state.data == null ? "" : this.state.data[0].cellPhone}
+						onChange={this.changePhone}
+						type="text"
+						id="phone"
+						name="phone"
+						autoComplete="off"
+					/>
+					<button id="phone" onClick={this.handleClick}>
+						編輯
+					</button>
 				</div>
 
 				<hr />
@@ -134,10 +145,13 @@ class User extends Component {
 				<div>
 					<label>性別：</label>
 					<span>
-					<input 
-						placeholder = {this.state.data == null ? "": this.state.data[0].gender}
-						readOnly  name="gender" 
-						type="text" autoComplete="off" />
+						<input
+							placeholder={this.state.data == null ? "" : this.state.data[0].gender}
+							readOnly
+							name="gender"
+							type="text"
+							autoComplete="off"
+						/>
 						{/* <label htmlFor="male">男生</label> */}
 						{/* <input 
 						checked={this.state.data == null ? "": this.state.data[0].gender}
@@ -152,27 +166,36 @@ class User extends Component {
 
 				<hr />
 
-				<div> 
+				<div>
 					<label htmlFor="date">生日：</label>
-					<input disabled value={moment(this.state.data == null ? "" : this.state.data[0].birth_date).format('YYYY-MM-DD')}
-					 name="date" />
+					<input
+						disabled
+						//value={moment(this.state.data == null ? "" : this.state.data[0].birth_date).format(
+						//	"YYYY-MM-DD"
+						//)}
+						name="date"
+					/>
 				</div>
 
-				<hr/>
+				<hr />
 
 				<div>
 					<label htmlFor="email">E-mail：</label>
 					<input
-						style = {{border:this.state.emailIsShow ?"0":"2px black solid"}}
-						disabled={this.state.emailDisabled} 
+						style={{ border: this.state.emailIsShow ? "0" : "2px black solid" }}
+						disabled={this.state.emailDisabled}
 						placeholder={this.state.data == null ? "" : this.state.data[0].email}
-						onChange = {this.changeEmail}
-						type="text" id="email" name="email" autoComplete="off"
+						onChange={this.changeEmail}
+						type="text"
+						id="email"
+						name="email"
+						autoComplete="off"
 					/>
-				<button id="email" onClick={this.handleClick}>編輯</button>
+					<button id="email" onClick={this.handleClick}>
+						編輯
+					</button>
 				</div>
 				<hr />
-
 			</div>
 		);
 	}
--- server/server.js
+++ server/server.js
@@ -260,142 +260,19 @@ app.post('/addCart', function (req, res) {
     }
 
 
-// `SELECT *
-// FROM category AS c 
-// INNER JOIN product AS p 
-// ON c.category_id = p.category_id 
+    // `SELECT *
+    // FROM category AS c 
+    // INNER JOIN product AS p 
+    // ON c.category_id = p.category_id 
 
-// INNER JOIN cart AS cart
-// ON p.product_id = cart.product_id
+    // INNER JOIN cart AS cart
+    // ON p.product_id = cart.product_id
 
-// INNER JOIN productimg AS pdimg
-// ON p.product_id = productImg_id
-// WHERE cart.customer_id
+    // INNER JOIN productimg AS pdimg
+    // ON p.product_id = productImg_id
+    // WHERE cart.customer_id
 
-//會員
-app.post('/member/:nickname', function (request, response) {
-  let sql = (
-    `select * 
-    from customer as customer
-    inner join orderdetail as o
-    on customer.customer_id = o.customer_id
-
-    inner join favorite as favo
-    on customer.customer_id = favo.customer_id
-
-    inner join coin as coin
-    on customer.customer_id = coin.customer_id
-    where nickname="${request.body.nickname}"`
-  );
-  conn.query(sql, function (err, rows) {
-    if (err) return;
-
-    if (rows.length == 0) {
-      //console.log(rows.length)
-      response.send([{ "info": "error" }]);
-    } else {
-      // console.log(rows)
-      response.send(rows);
-    }
-  });
-})
-
-// 購買清單
-app.post('/memberbuy/', function (request, response) {
-  let sql = (
-    `select * 
-    from customer as customer
-    inner join orderdetail as o
-    on customer.customer_id = o.customer_id
- 
-    inner join cart as cart
-    on customer.customer_id = cart.customer_id
-
-    inner join product as p
-    on cart.product_id = p.product_id
-
-    inner join category as cat
-    on cat.category_id = p.category_id
-    where nickname="${request.body.nickname}"
-    GROUP BY o.orderDetail_id`
-  );
-  conn.query(sql, function (err, rows) {
-    if (err) return;
-    
-    // console.log(rows.length)
-    if (rows.length == 0) {
-      response.send([{ "info": "error" }]);
-    } else {
-      // console.log(rows)
-      response.send(rows);
-    }
-  });
-})
-
-
-// 收藏
-app.post('/memberfavorite/', function (request, response) {
-  let sql = (
-    `select * 
-    from customer as customer
-    inner join favorite as f
-    on f.customer_id = customer.customer_id
-
-    inner join product as p
-    on p.product_id = f.product_id
- 
-    inner join category as cat
-    on cat.category_id = p.category_id
-
-    inner join cart as cart
-    on customer.customer_id = cart.customer_id
-
-    where nickname="${request.body.nickname}"
-    GROUP BY f.favorite_id`
-  );
-  
-  conn.query(sql, function (err, rows) {
-    // console.log(rows)
-    if (err) return;
-    
-    // console.log(rows.length)
-    if (rows.length == 0) {
-      response.send([{ "info": "error" }]);
-    } else {
-      // console.log(rows)
-      response.send(rows);
-    }
-  });
-})
-
-
-// 星幣賺取
-app.post('/membercoin/', function (request, response) {
-  let sql = (
-    `select * 
-    from customer as customer
-    inner join coin as c
-    on c.customer_id = customer.customer_id
-
-    where nickname="${request.body.nickname}"
-    GROUP BY c.coin_id`
-  );
-  
-  conn.query(sql, function (err, rows) {
-    // console.log(rows)
-    if (err) return;
-    
-    // console.log(rows.length)
-    if (rows.length == 0) {
-      response.send([{ "info": "error" }]);
-    } else {
-      // console.log(rows)
-      response.send(rows);
-    }
-  });
-})
-
-// 刪除
+    // 刪除
     //如果沒有此商品則加入
     if (rows.length == 0) {
       // console.log(rows.length);
@@ -449,37 +326,123 @@ app.post('/updateQty', function (req, res) {
   });
 });
 
-//編輯
-app.get('/detail/:id([0-9]+)', function (req, res) {
-  var sql = `SELECT customer_id,nickname,cellPhone,city FROM customer WHERE customer_id = ?;`;
-  var data = [req.params.id];
-  db.exec(sql, data, function (results, fields) {
-    if (results[0]) {
-      res.end(JSON.stringify(new Success(results[0])));
-    } else {
-      res.end(JSON.stringify(new Error('no result')));
-    }
-  });
-});
+//會員
+app.post('/member/:nickname', function (request, response) {
+  let sql = (
+    `select * 
+    from customer as customer
+    inner join orderdetail as o
+    on customer.customer_id = o.customer_id
 
-app.post('/update', function (req, res) {
-  var body = req.body
-  // console.log(body);
-  var sql = `UPDATE customer SET customer_id = ?, nickname = ?, cellPhone = ?, city = ? WHERE customer_id = ?`;
-  var data = [
-    parseInt(body.id),
-    body.name,
-    body.phone,
-    body.address,
-    parseInt(body.id),
-  ];
-  db.exec(sql, data, function (results, fields) {
-    if (results.affectedRows) {
-      res.end(JSON.stringify(new Success('update success')));
+    inner join favorite as favo
+    on customer.customer_id = favo.customer_id
+
+    inner join coin as coin
+    on customer.customer_id = coin.customer_id
+    where customer.customer_id=${request.body.cId}
+    group by customer.customer_id`
+  );
+  conn.query(sql, function (err, rows) {
+    if (err) return;
+
+    console.log(rows)
+    response.send(rows);
+  });
+})
+
+// 購買清單
+app.post('/memberbuy/', function (request, response) {
+  let sql = (
+    `select * 
+    from customer as customer
+    inner join orderdetail as o
+    on customer.customer_id = o.customer_id
+ 
+    inner join cart as cart
+    on customer.customer_id = cart.customer_id
+
+    inner join product as p
+    on cart.product_id = p.product_id
+
+    inner join category as cat
+    on cat.category_id = p.category_id
+    where nickname="${request.body.nickname}"
+    GROUP BY o.orderDetail_id`
+  );
+  conn.query(sql, function (err, rows) {
+    if (err) return;
+
+    // console.log(rows.length)
+    if (rows.length == 0) {
+      response.send([{ "info": "error" }]);
     } else {
-      res.end(JSON.stringify(new Error('update failed')));
+      // console.log(rows)
+      response.send(rows);
     }
   });
-});
+})
+
+
+// 收藏
+app.post('/memberfavorite/', function (request, response) {
+  let sql = (
+    `select * 
+    from customer as customer
+    inner join favorite as f
+    on f.customer_id = customer.customer_id
+
+    inner join product as p
+    on p.product_id = f.product_id
+ 
+    inner join category as cat
+    on cat.category_id = p.category_id
+
+    inner join cart as cart
+    on customer.customer_id = cart.customer_id
+
+    where nickname="${request.body.nickname}"
+    GROUP BY f.favorite_id`
+  );
+
+  conn.query(sql, function (err, rows) {
+    // console.log(rows)
+    if (err) return;
+
+    // console.log(rows.length)
+    if (rows.length == 0) {
+      response.send([{ "info": "error" }]);
+    } else {
+      // console.log(rows)
+      response.send(rows);
+    }
+  });
+})
+
+
+// 星幣賺取
+app.post('/membercoin/', function (request, response) {
+  let sql = (
+    `select * 
+    from customer as customer
+    inner join coin as c
+    on c.customer_id = customer.customer_id
+
+    where nickname="${request.body.nickname}"
+    GROUP BY c.coin_id`
+  );
+
+  conn.query(sql, function (err, rows) {
+    // console.log(rows)
+    if (err) return;
+
+    // console.log(rows.length)
+    if (rows.length == 0) {
+      response.send([{ "info": "error" }]);
+    } else {
+      // console.log(rows)
+      response.send(rows);
+    }
+  });
+})
 
 app.listen(3001, () => console.log('LISTENING ON PORT 成功'));
